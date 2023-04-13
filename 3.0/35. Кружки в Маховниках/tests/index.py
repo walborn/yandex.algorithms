b4ed7600bde530b7f50edf059c209c2c1849f2aa
Boris Yuzhakov <yuzhakov.boris@gmail.com>
2023-04-13 21:07:46 +0300

    fix structure

--- 3.0/35. Кружки в Маховниках/tests/index.py
+++ 3.0/35. Кружки в Маховниках/tests/index.py
@@ -4,7 +4,7 @@ def rel(filename): return path.join(path.dirname(__file__), filename)
 
 __open__ = open
 __test__ = 1
-tests = {i: __open__(rel(str(i))).read().split('\n') for i in range(1, 4)}
+tests = {i: __open__(rel(str(i))).read().split('\n') for i in range(2)}
 
 def input(): return tests[__test__].pop(0)
 
