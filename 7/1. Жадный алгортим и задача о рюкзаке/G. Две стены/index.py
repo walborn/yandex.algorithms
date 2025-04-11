796a3bb08de7f40dabdeccb3c89793abed361fac
walborn <yuzhakov.boris@gmail.com>
2025-04-11 17:09:04 +0300

    G. Две стены

--- 7/1. Жадный алгортим и задача о рюкзаке/G. Две стены/index.py
+++ 7/1. Жадный алгортим и задача о рюкзаке/G. Две стены/index.py
@@ -1,35 +1,39 @@
 from tests.index import test, input
-test(3)
-  
+test(1)
 
 n, k = map(int, input().split())
-bricks = [[] for _ in range(k)]
+rows = [[] for _ in range(k)]
+lens = [0]
 
-for i in range(n):
+for i in range(1, n + 1):
   w, j = map(int, input().split())
-  bricks[j - 1].append(w)
+  lens.append(w)
+  rows[j - 1].append((w, i))
   
-width = sum(bricks[0])
-a = [True] * (width + 1)
+m = sum([ w for w, i in rows[0] ])
+a = [True] * (m + 1)
 
 d = []
-for lens in bricks:
-  b = [0] + [-1] * width
-  for w in lens:
-    for i in range(width - w, -1, -1):
-      if b[i] != -1 and b[i + w] == -1:
-        b[i + w] = w
-  print(*b)
+for row in rows:
+  b = [0] + [-1] * m
+  for w, i in row:
+    for j in range(m - w, -1, -1):
+      if b[j] != -1 and b[j + w] == -1:
+        b[j + w] = i
+  
   a = [i and j != -1 for i, j in zip(a, b)]
   d.append(b)
 
+try:
+  i = a.index(True, 1, -1)
+  ans = []
+  for b in d:
+    j = i
+    while j != 0:
+      ans.append(b[j])
+      j -= lens[b[j]]
     
-for i in range(1, width + 1):
-  if a[i]:
-    print('YES')
-    print(i)
-    for 
-    print(*d)
-    break
-else:
+  print('YES')
+  print(*ans)
+except ValueError:
   print('NO')
