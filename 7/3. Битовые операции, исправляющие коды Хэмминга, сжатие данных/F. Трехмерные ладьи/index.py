fe7e8ca0c1cc21d04c696b69289c2321f13ffd64
walborn <yuzhakov.boris@gmail.com>
2025-04-28 18:13:44 +0300

    black

---
+++ 7/3. Битовые операции, исправляющие коды Хэмминга, сжатие данных/F. Трехмерные ладьи/index.py
@@ -0,0 +1,13 @@
+from tests.index import test, input
+test(1)
+
+n, k = map(int, input().split())
+rooks = [map(int, input().split()) for i in range(k)]
+
+for x, y, z in rooks:
+  yz.add(x)
+  zx.add(y)
+  xy.add(z)
+
+
+
